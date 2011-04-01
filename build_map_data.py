d04a40b6391b4a4ec858b375474f380607842a43
Bryan Koroleski <korol1bp@cmich.edu>
2011-04-01 19:44:34 +0000

    Need to exclude dot files for our friends using SVN.
    git-svn-id: https://udbraaains.googlecode.com/svn/trunk@42 79255bfa-b748-0410-9b63-11db1765da97

--- build_map_data.py
+++ build_map_data.py
@@ -1,3 +1,5 @@
+"""Builds map_data required by the server to function correctly."""
+
 import re
 import os
 import lxml.html
@@ -7,7 +9,7 @@ LINE = '{building_type} {x} {y} {building_name}\n'
 
 def main():
     with open('map_data', 'w') as map_data:
-        for suburb_file in os.listdir('suburbs'):
+        for suburb_file in [path for path in os.listdir('suburbs') if not path.startswith('.'):
             suburb = lxml.html.parse(os.path.join('suburbs', suburb_file))
             for cell in suburb.xpath('/html/body/table/tr/td/table//td'):
                 lines = cell.text_content().strip().split('\n\n')
