9a0dd18a1f85d271fb596b9c53dc42a26405e55d
Bryan Koroleski <korol1bp@cmich.edu>
2011-04-01 19:25:21 +0000

    Added a script to build map data.
    git-svn-id: https://udbraaains.googlecode.com/svn/trunk@41 79255bfa-b748-0410-9b63-11db1765da97

---
+++ build_map_data.py
@@ -0,0 +1,23 @@
+import re
+import os
+import lxml.html
+
+COORD = re.compile(r'\[(\d+), (\d+)\]')
+LINE = '{building_type} {x} {y} {building_name}\n'
+
+def main():
+    with open('map_data', 'w') as map_data:
+        for suburb_file in os.listdir('suburbs'):
+            suburb = lxml.html.parse(os.path.join('suburbs', suburb_file))
+            for cell in suburb.xpath('/html/body/table/tr/td/table//td'):
+                lines = cell.text_content().strip().split('\n\n')
+                coords = COORD.search(lines[1])
+                line = LINE.format(building_type=cell.attrib['class'][:-1], 
+                    x=coords.group(1), 
+                    y=coords.group(2), 
+                building_name=lines[0])
+                map_data.write(line)
+
+
+if __name__ == '__main__':
+    main()
