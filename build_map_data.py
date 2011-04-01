1b8273764a6b9b4603772901467ed06f37fa37ad
Bryan Koroleski <korol1bp@cmich.edu>
2011-04-01 19:46:13 +0000

    I should try running things before committing them :(
    git-svn-id: https://udbraaains.googlecode.com/svn/trunk@43 79255bfa-b748-0410-9b63-11db1765da97

--- build_map_data.py
+++ build_map_data.py
@@ -9,7 +9,7 @@ LINE = '{building_type} {x} {y} {building_name}\n'
 
 def main():
     with open('map_data', 'w') as map_data:
-        for suburb_file in [path for path in os.listdir('suburbs') if not path.startswith('.'):
+        for suburb_file in [path for path in os.listdir('suburbs') if not path.startswith('.')]:
             suburb = lxml.html.parse(os.path.join('suburbs', suburb_file))
             for cell in suburb.xpath('/html/body/table/tr/td/table//td'):
                 lines = cell.text_content().strip().split('\n\n')
