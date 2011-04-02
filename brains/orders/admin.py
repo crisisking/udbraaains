4c75bc9d93026c567601491ccdc025abde418dba
Bryan Koroleski <korol1bp@cmich.edu>
2011-04-02 04:38:02 +0000

    Created a simple Django app to serve up orders to the horde.
    git-svn-id: https://udbraaains.googlecode.com/svn/trunk@56 79255bfa-b748-0410-9b63-11db1765da97

---
+++ brains/orders/admin.py
@@ -0,0 +1,11 @@
+from orders.models import Order
+from django.contrib import admin
+
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = ('user', 'date')
+    
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        obj.save()
+
+admin.site.register(Order, OrderAdmin)
---
+++ brains/orders/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from orders.models import Order
+
+def index(request):
+    return render_to_response('orders/orders.html', 
+        { 'orders': Order.objects.all().order_by('date')[0:2] },
+        context_instance=RequestContext(request)
+    )
