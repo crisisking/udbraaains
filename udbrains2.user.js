4db2f75aef42e58fa38c7db217bef334d8168fe3
Brandon <brandon@aluminium.local>
2011-04-04 08:52:41 +0800

    Splitting current script into two, the userscript injects the other into the page.

---
+++ udbrains2.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name          UDBraaains
+// @namespace     http://code.google.com/p/udbraaains/
+// @description   Reports and downloads metadata for Urban Dead
+// @include       http://*urbandead.com/map.cgi*
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// ==/UserScript==
+
+(function ($) {
+
+
+   function appendScript(source, loadHandler) {
+   	var head = document.getElementsByTagName('head')[0] || document.documentElement;
+   	var script = document.createElement('script');
+
+   	script.src = source;
+   	script.type = 'text/javascript';
+   	script.addEventListener('load', loadHandler, false);
+   	head.appendChild(script);
+   }
+   function reportData (data, reporturl) {
+      GM_xmlhttpRequest({
+         method: "POST",
+         url: reporturl,
+         data: data,
+         onload: function () {
+            
+         }
+      });
+   }
+   
+   appendScript('http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js', function () {
+      appendScript('http://localhost:8000/udbraaains.js', function () {
+         
+      });      
+   });
+   
+   
+
+
+   
+   
+})(jQuery);
