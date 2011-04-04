33556b6761b791144cba1c28a51cb243004bcbcc
Bryan Koroleski <korol1bp@cmich.edu>
2011-04-04 15:55:19 -0400

    Moved brains server to new host.

--- udbraaains.js
+++ udbraaains.js
@@ -566,7 +566,7 @@ function exchangeData() {
 		method: 'POST',
 //		url: 'http://www.alloscomp.com/udbrain/api2.php'+qs,
 //		url: 'http://127.0.0.1:8080/udb'+qs,
-		url: 'http://udbrains.kimihia.org.nz:50609/udb'+qs,
+		url: 'http://old.somethingdead.com/udb'+qs,
 		headers: {
 			"Accept": "text/plain",
 			"Content-type": "application/x-www-form-urlencoded"
