90985041743d5878f95f7fc95f613f37dabea2e3
Bryan Koroleski <korol1bp@cmich.edu>
2011-04-04 22:47:54 -0400

    Spruce up the admin interface a little bit.

--- brains/namelist/admin.py
+++ brains/namelist/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from namelist.models import Category, Player
 
 
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category']
+    list_filter = ['category', 'group_name']
+
 admin.site.register(Category)
-admin.site.register(Player)
+admin.site.register(Player, PlayerAdmin)
