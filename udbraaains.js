cbb6a9fb068f222ad88e50f8520dbfc054c618dc
Bryan Koroleski <korol1bp@cmich.edu>
2011-04-05 16:34:36 -0400

    Added color coding back in.

--- udbraaains.js
+++ udbraaains.js
@@ -832,6 +832,20 @@ gUDID = getUDID();
 
 if(gUDID != -1) {
 //	alert(timezone);
+
+    checkSurvivors();
+    player_payload = {players:[]};
+    
+    for(var i=0; i < gSurvivorIds.length; i += 1) {
+        player_payload.players.push(gSurvivorIds[i][0]);
+    }
+    
+    jQuery.post('http://brains.somethingdead.com/names/colors/', player_payload, function (data) {
+        for(var i=0; i < data.length; i += 1) {
+            jQuery('a[href="profile.cgi?' + data.id + '"]').css('color', data.color_code);
+        }
+    });
+    
 	gPlayerLocation = playerLocation();
 	if (gPlayerLocation != -1)
 	{
