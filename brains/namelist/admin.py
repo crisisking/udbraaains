8d508b43e2f4472b735029637a5eab8fcf37d307
Bryan Koroleski <korol1bp@cmich.edu>
2011-04-05 23:37:18 -0400

    Added some search stuff to the admin.

--- brains/namelist/admin.py
+++ brains/namelist/admin.py
@@ -5,6 +5,7 @@ from namelist.models import Category, Player
 class PlayerAdmin(admin.ModelAdmin):
     list_display = ['name', 'category']
     list_filter = ['category', 'group_name']
+    search_fields = ['category__name', 'name', 'group_name']
 
 admin.site.register(Category)
 admin.site.register(Player, PlayerAdmin)
