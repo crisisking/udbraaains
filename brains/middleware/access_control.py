3b23eb1a4a4ed0b0b4e693bb2ce17922d9b34a76
Bryan Koroleski <korol1bp@cmich.edu>
2011-04-06 07:07:21 -0400

    We need that options request to succeed, no matter what.

--- brains/middleware/access_control.py
+++ brains/middleware/access_control.py
@@ -3,9 +3,14 @@ from django.http import HttpResponse
 class AccessControl(object):
     
     def process_response(self, request, response):
+        if request.method == 'OPTIONS':
+            response = HttpResponse(status=200)
+
         if not request.path.startswith('/admin/') and request.META.has_key('HTTP_ORIGIN'):
             origin = request.META['HTTP_ORIGIN']
             if origin in ('http://urbandead.com', 'http://www.urbandead.com'):
                 response['Access-Control-Allow-Origin'] = origin
+                response['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+
         return response
 
