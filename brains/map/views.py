c2745340cc5eac51b5647bc47164e10ba708fe90
Bryan Koroleski <korol1bp@cmich.edu>
2011-04-07 23:49:58 -0400

    Forgot to stringify the date before serializing to JSON.

--- brains/map/views.py
+++ brains/map/views.py
@@ -10,7 +10,7 @@ REDIS = redis.Redis(db=4)
 @csrf_exempt
 def receive_data(request):
     if request.method == 'POST' and request.POST.has_key('data'):
-        data = {'date': datetime.datetime.now(), 'payload': request.POST['data']}
+        data = {'date': unicode(datetime.datetime.now()), 'payload': request.POST['data']}
         REDIS.lpush(json.dumps(data))
         return HttpResponse(status=200)
     return HttpResponse(status=405)
