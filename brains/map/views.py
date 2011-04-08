63763cc0d1f6c38eb1a155b818e6636f79a4b8b3
Bryan Koroleski <korol1bp@cmich.edu>
2011-04-07 23:51:34 -0400

    Tell redis which list to push to.

--- brains/map/views.py
+++ brains/map/views.py
@@ -11,6 +11,6 @@ REDIS = redis.Redis(db=4)
 def receive_data(request):
     if request.method == 'POST' and request.POST.has_key('data'):
         data = {'date': unicode(datetime.datetime.now()), 'payload': request.POST['data']}
-        REDIS.lpush(json.dumps(data))
+        REDIS.lpush('data-cache', json.dumps(data))
         return HttpResponse(status=200)
     return HttpResponse(status=405)
