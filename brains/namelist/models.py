6ecf0664b64155a95343f22f2a8311a235b37e6f
Bryan Koroleski <korol1bp@cmich.edu>
2011-04-09 17:52:41 -0400

    Admin speedups.

--- brains/namelist/models.py
+++ brains/namelist/models.py
@@ -18,7 +18,7 @@ class Player(models.Model):
     
     name = models.CharField(max_length=20, null=False)
     profile_id = models.IntegerField(null=False, unique=True)
-    group_name = models.CharField(max_length=50, blank=False)
+    group_name = models.CharField(max_length=50, blank=False, db_index=True)
     category = models.ForeignKey(Category, null=True, blank=True)
     join_date = models.DateTimeField(default=datetime.datetime.now)
     scrape_date = models.DateTimeField(auto_now=True, auto_now_add=True)
