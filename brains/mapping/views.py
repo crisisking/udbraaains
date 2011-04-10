5cf7b30875699f0baf5af8bf43d7e82f85e6485a
Bryan Koroleski <korol1bp@cmich.edu>
2011-04-10 19:02:22 -0400

    Reworked view to not process annotations on demand.

--- brains/mapping/views.py
+++ brains/mapping/views.py
@@ -5,6 +5,9 @@ from django.views.decorators.csrf import csrf_exempt
 from mapping.models import Location, Report
 from mapping.tasks import process_data, get_player
 from namelist.models import Category, Player
+import redis
+
+CONN = redis.Redis(db=6)
 
 
 @csrf_exempt
@@ -15,72 +18,21 @@ def receive_data(request):
         origin_x = data['surroundings']['position']['coords']['x']
         origin_y = data['surroundings']['position']['coords']['y']
         ip = request.META['HTTP_X_REAL_IP']
-        data['surroundings']['position']['survivors'].append({
-            'name': data['user']['name'],
-            'id': data['user']['id'],
-        })
+
         process_data.delay(data, ip)
 
         data = []
-        
+
         # Grab all locations in a 15x15 square, centered on the player's position
         x_range = range(origin_x-1, origin_x+7) + range(origin_x, origin_x-7, -1)
         y_range = range(origin_y-1, origin_y+7) + range(origin_y, origin_y-7, -1)
-        locations = Location.objects.filter(x__in=x_range, 
-                                            y__in=y_range)
 
-        for location in locations:
+        for x in x_range:
+            for y in y_range:
+                annotation = CONN.get('location:{0}:{1}'.format(x, y))
+                if annotation:
+                    data.append(json.loads(annotation))
 
-            annotation = {}
-            annotation['x'] = location.x
-            annotation['y'] = location.y
-            
-            # Grab all reports from the last five days
-            reports = location.report_set.exclude(reported_date__lte=datetime.datetime.now() - datetime.timedelta(days=5))
-            reports = reports.order_by('-reported_date')
-            
-            # For barricade info, ignore the reports without levels set
-            barricades = reports.exclude(barricade_level=None)
-            if barricades:
-                    annotation['barricades'] = barricades[0].barricade_level
-            else:
-                annotation['barricades'] = None
-
-            # Grab report stats if they exist
-            annotation['survivor_count'] = location.player_set.filter(is_dead=False).count()
-            if reports:
-                report = reports[0]
-                annotation['ruined'] = report.is_ruined
-                annotation['illuminated'] = report.is_illuminated
-                annotation['zombies'] = report.zombies_present
-                annotation['report_age'] = unicode(datetime.datetime.now() - report.reported_date)
-            else:
-                annotation['ruined'] = None
-                annotation['illuminated'] = None
-                annotation['zombies'] = None
-                annotation['report_date'] = None
-            data.append(annotation)
-            
-        # Find all reported Christmas trees for smashing
-        christmas_trees = Location.objects.filter(has_tree=True).values('x', 'y').distinct()
-        trees = []
-        for location in christmas_trees:
-            trees.append({'x': location['x'], 'y': location['y']})
-
-        # Get all priority targets and other worthless players
-        priority_targets = Player.objects.exclude(category=None).exclude(category__name=u"Goon")
-        priority_targets = priority_targets.exclude(location=None)
-        priority_targets = priority_targets.values_list('location__x', 'location__y', 'name', 'profile_id', 'category__color_code')
-        targets = []
-        for target in priority_targets:
-            targets.append({
-                'x': target[0],
-                'y': target[1],
-                'name': target[2],
-                'id': target[3],
-                'color': target[4],
-            })
-
-        return HttpResponse(json.dumps({'annotation': data, 'trees':trees, 'targets':targets }), content_type='application/json', status=200)
+        return HttpResponse(json.dumps({'annotation': data }), content_type='application/json', status=200)
 
     return HttpResponse(status=405)
