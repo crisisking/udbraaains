477031e969d705456be5a757314b291ebf3c75e2
Bryan Koroleski <bryan.koroleski@gmail.com>
2011-10-24 01:11:04 -0500

    Chrome's getting angry about cross domain requests again.

--- brains/middleware/access_control.py
+++ brains/middleware/access_control.py
@@ -1,4 +1,9 @@
+import re
 from django.http import HttpResponse
+
+
+ALLOWED_PATTERN = re.compile(r'http://(www\.)urbandead\.com')
+
 
 class AccessControl(object):
     
@@ -6,9 +11,9 @@ class AccessControl(object):
         if request.method == 'OPTIONS':
             response = HttpResponse(status=200)
 
-        if not request.path.startswith('/admin/') and request.META.has_key('HTTP_ORIGIN'):
+        if not request.path.startswith('/admin/') and 'HTTP_ORIGIN' in request.META:
             origin = request.META['HTTP_ORIGIN']
-            if origin in ('http://urbandead.com', 'http://www.urbandead.com'):
+            if ALLOWED_PATTERN.search(origin):
                 response['Access-Control-Allow-Origin'] = origin
                 response['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
 
