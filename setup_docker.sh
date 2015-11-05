fafd9ad93ae9b02be310d5f1e150ddb6567e84d2
Bryan Koroleski <bryan.koroleski@gmail.com>
2015-11-05 17:22:15 -0600

    Add a redis server.

--- setup_docker.sh
+++ setup_docker.sh
@@ -3,11 +3,12 @@ eval "$(docker-machine env brains)"
 
 docker build -t brains/base -f docker/base.docker .
 docker build -t brains/postgres -f docker/postgres.docker .
+docker build -t brains/redis -f docker/redis.docker .
 docker build -t brains/python_base -f docker/build_python.docker .
 docker build -t brains/project_base -f docker/build_project.docker .
 docker build -t brains/django -f docker/django_app.docker .
 
 docker run --name postgres_data brains/postgres echo 'Hello from postgres!'
 docker run -d --name db1 -p 5432:5432 --volumes-from postgres_data brains/postgres
-docker run --name python brains/python_base echo 'hi from python!'
-docker run --name django -p 8000:8000 --volumes-from project_base --volumes-from python brains/django
+docker run -d --name redis -p 6379:6379 brains/redis
+docker run --name python brains/project_base echo 'hi from python!'
